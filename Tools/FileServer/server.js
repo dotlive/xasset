66781b4e7547af206e91c27e7cf9082202040e60
dotlive <dotlive@163.com>
2020-07-19 08:04:13 +0800

    添加node版的资源服务器;

---
+++ Tools/FileServer/server.js
@@ -0,0 +1,15 @@
+﻿// 使用express模块
+
+var express = require("express");
+var app = express();
+ 
+// 指定etag
+app.set("etag", "weak");
+// 指定静态资源访问目录
+app.use(express.static("../../"));
+ 
+var server = app.listen(7888, "127.0.0.1", function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Server running at http://%s:%s", host, port);
+});
